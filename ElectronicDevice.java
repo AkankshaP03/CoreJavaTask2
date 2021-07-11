ca2c7f7a2dbc70d3ffe8016a2cf626bcfdab9840
AkankshaP03 <55937868+AkankshaP03@users.noreply.github.com>
2021-07-12 00:52:06 +0530

    Update ElectronicDevice.java

--- ElectronicDevice.java
+++ ElectronicDevice.java
@@ -3,25 +3,26 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package p5;
+package Task2;
+import java.util.Scanner;
 
-/**
- *
- * @author mukul
- */
-public class ElectronicDevice {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        Laptop lap =  new Laptop(100);
-       Computer cmp = new Computer(200);
-        Camera cam = new Camera(100);
-        lap.getpower();
-        cmp.getpower();
-        cam.getpower();
-    }
-    
+public class ElectronicDevices {
+	public static void main(String[] args) {
+		Scanner src=new Scanner(System.in);
+		System.out.println("Enter Power Usage of Camera:");
+		int campow=Integer.parseInt(src.nextLine());
+		System.out.println("Enter Power Usage of Laptop:");
+		int lappow=Integer.parseInt(src.nextLine());
+		System.out.println("Enter Power Usage of Computer:");
+		int comppow=Integer.parseInt(src.nextLine());
+		Camera cam =new Camera(campow);
+		Laptop lap =new Laptop(lappow);
+		Computer comp =new Computer(comppow);
+		cam.getPower();
+		lap.getPower();
+		comp.getPower();
+		
+	}
+
 }
